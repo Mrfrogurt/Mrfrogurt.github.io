85307a2e73133bad3544b10a171e02fc7ca3602b
Mrfrogurt <georgedoucy@gmail.com>
2017-09-20 19:58:05 +0100

    First update

---
+++ main.js
@@ -0,0 +1,27 @@
+function createWard(ward, mouseEvent) {
+	const origAudio = document.getElementById(ward+"-audio");
+	const newAudio = origAudio.cloneNode()
+	const origImage = document.getElementById(ward+"-image");
+	const newImage = origImage.cloneNode();
+
+	newAudio.play()
+
+	newImage.style.top = (mouseEvent.clientY - 198) + "px";
+	newImage.style.left = (mouseEvent.clientX - 143) + "px";
+	newImage.classList.remove("preload");
+	const body = document.getElementsByTagName("body")[0];
+	body.appendChild(newImage);
+}
+
+function click(mouseEvent) {
+	const percentChanceSquire = 0.25;
+	let ward;
+	
+	ward = "VenoWard";
+	
+	
+	
+	createWard(ward, mouseEvent);
+}
+
+document.addEventListener("click", click);
